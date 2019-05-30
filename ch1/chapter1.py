9f9d655c25d70521c2ffba9680ba74c6d36783a2
chenruicool <51191546+chenruicool@users.noreply.github.com>
2019-05-30 16:26:20 +0800

    Add files via upload

---
+++ ch1/chapter1.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+# coding=utf-8
+
+class Test():
+	def __init__(self, name):
+		self.name = name
+
+	def print_name(self):
+		print self.name
+
+aList = []
+for i in range(10):
+	test = Test('i=' + str(i))
+	aList.append(test)
+
+# aList.print_name()
+
+print 'c'.find('s')
